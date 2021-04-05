7a59b431f674fd85a80eba5f9843c6d5e7a6f02e
Laptop - Omar <omar.angarita.j@gmail.com>
2021-04-04 21:09:28 -0500

    añadiendo validaciones Creando ROL

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,20 +1,47 @@
-const { response } = require('express')
+const { request, response } = require('express')
+const bcryptjs = require("bcryptjs");
+const User = require('../models/user.models');
+const Role = require('../models/role.models');
 
-const usersGet = (req, res = response) => {
+
+const usersGet = (req = request, res = response) => {
     res.status(200).json({ method: "GET" });
 }
-const usersPost = (req, res = response) => {
-    const body = req.body;
-    res.status(201).json({ method: "POST" });
+
+
+const usersPost = (req = request, res = response) => {
+
+    const salt = bcryptjs.genSaltSync();
+    const { name, email, password, role } = req.body
+
+    const user = new User({ name, email, password, role });
+    user.password = bcryptjs.hashSync(password, salt);
+
+    user.save();
+    res.status(201).json({ user });
 }
-const usersPut = (req, res = response) => {
+
+
+const usersPut = (req = request, res = response) => {
     res.status(202).json({ method: "PUT" });
 }
-const usersPatch = (req, res = response) => {
+
+
+const usersPatch = (req = request, res = response) => {
     res.status(202).json({ method: "PATCH" });
 }
-const usersDelete = (req, res = response) => {
+
+
+const usersDelete = (req = request, res = response) => {
     res.status(202).json({ method: "DELETE" });
+}
+
+const rolePost = (req = request, res = response) => {
+    let { role } = req.body;
+    role = role.toUpperCase();
+    const role_May = new Role({ role });
+    role_May.save();
+    res.status(201).json({ msg: "Role created", role: role_May });
 }
 
 module.exports = {
@@ -22,5 +49,6 @@ module.exports = {
     usersPost,
     usersPut,
     usersPatch,
-    usersDelete
+    usersDelete,
+    rolePost
 };
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,21 +1,42 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validacionCampos } = require("../middleware/validacion-campos");
 const {
     usersGet,
     usersPost,
     usersPut,
     usersPatch,
-    usersDelete
+    usersDelete,
+    rolePost
 } = require('../controllers/users.controller')
+
+const Role = require('../models/role.models')
 
 
 const router = Router();
 
 router.get("/", usersGet);
-router.post("/", usersPost);
+router.post(
+    "/", [
+        check("name", "The name isnt empty.").not().isEmpty(),
+        check("email", "The email isnt valid.").isEmail(),
+        check("password", "The password hasnt have 6 digits").isLength({ min: 6 }),
+        // check("role", "The role isnt valid.").isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        check("role").custom(async(role = '') => {
+            const existRole = await Role.findOne({ role });
+            if (!existRole) {
+                throw new Error(`The role ${role} isnt exist`);
+            }
+        }),
+        validacionCampos
+    ],
+    usersPost
+);
 router.put("/", usersPut);
 router.patch("/", usersPatch);
 router.delete("/", usersDelete);
 
+router.post("/role", rolePost);
 
 
 module.exports = router
